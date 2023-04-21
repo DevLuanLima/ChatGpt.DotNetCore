a05114b283c962a6eff8629dbb3524d0f34d4fcb
dev.lima <luan_marcell@outlook.com>
2023-04-21 16:35:01 -0300

    ConsoleApp and Api projects

---
+++ chatConsole/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using OpenAI_API;
+using OpenAI_API.Completions;
+
+namespace chatConsole
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Key Configuration
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var openAIConfig = config.GetSection("OpenAI");
+            var apiKey = openAIConfig.GetValue<string>("ApiKey");
+
+            //Object
+            var openai = new OpenAIAPI(apiKey);
+
+            // Loop
+            while (true)
+            {
+
+                Console.Write("Você: ");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.BackgroundColor = ConsoleColor.Blue;
+
+                var input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input))
+                    continue;
+
+                if (input == "\u001b") // ESC
+                    break;
+
+                var request = new CompletionRequest
+                {
+                    Model = OpenAI_API.Models.Model.DavinciText,
+                    Prompt = input,
+                    Temperature = 0.5,
+                    MaxTokens = 120
+                };
+
+                var completions = await openai.Completions.CreateCompletionAsync(request);
+                var response = completions.Completions[0].Text;
+
+                Console.BackgroundColor = ConsoleColor.DarkGreen;
+                Console.Write("ChatGpt: ");
+                Console.BackgroundColor = ConsoleColor.DarkGreen;
+
+                Console.WriteLine(response);
+
+                Console.ResetColor();
+            }
+        }
+    }
+}
---
+++ chatgptApi/Controllers/GPTController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OpenAI_API;
+using OpenAI_API.Completions;
+
+namespace chatgpt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GPTController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> UseChatGpt(string query)
+        {
+            string outPutResult = "";
+
+            //API Configuration
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var openAIConfig = config.GetSection("OpenAI");
+            var apiKey = openAIConfig.GetValue<string>("ApiKey");
+
+            // API Object
+            var openai = new OpenAIAPI(apiKey);
+
+            CompletionRequest completionRequest = new CompletionRequest();
+            completionRequest.Prompt = query;
+            completionRequest.Model = OpenAI_API.Models.Model.DavinciText;
+
+            //Send solicitation and wait answer
+            var completions = await openai.Completions.CreateCompletionAsync(completionRequest);
+
+            foreach (var completion in completions.Completions)
+            {
+                outPutResult += completion.Text;
+            }
+            //Result
+            return Ok(outPutResult);
+        }
+    }
+}
